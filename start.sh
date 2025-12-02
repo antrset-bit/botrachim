13077e54fdd52da6d57e247f78b50655f4f40aa3
antrset-bit <antrset@gmail.com>
2025-12-02 18:18:04 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,11 +4,12 @@ set -euo pipefail
 uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}" --workers 1 &
 API_PID=$!
 
-python -u -m app.bot &
-BOT_PID=$!
+# ВРЕМЕННО отключаем бота
+# python -u -m app.bot &
+# BOT_PID=$!
 
 python -u -m app.reminders &
 REM_PID=$!
 
-trap "kill $API_PID $BOT_PID $REM_PID || true" TERM INT
-wait -n $API_PID $BOT_PID $REM_PID
+trap "kill $API_PID $REM_PID || true" TERM INT
+wait -n $API_PID $REM_PID
